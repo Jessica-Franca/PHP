3a5dd2f291f003bf9c8c8d8f0c6a6893482c2402
Jessica-Franca <jealunaifsp@hotmail.com>
2023-07-29 19:57:44 -0300

    Ajuste tela de solicitação de férias

--- paginas/SolicitacaoFerias.php
+++ paginas/SolicitacaoFerias.php
@@ -6,16 +6,16 @@
 </head>
 
 <body>
-    <?php include_once('navbar.php'); ?>
+    <?php include_once('navbar.php'); ?><br>
 
-    <h1>Solicitação de Férias e Décimo Terceiro</h1>
     <div class="container">
         <form action="processar_solicitacao.php" method="POST">
+            <h1>Solicitação de Férias e Décimo Terceiro</h1><br>
 
             <div class="mb-3">
                 <label for="formGroupExampleInput" class="form-label">Nome completo</label>
                 <input type="text" class="form-control" id="formGroupExampleInput"
-                    placeholder="Escreva seu nome completo" required><br><br>
+                    placeholder="Escreva seu nome completo" required><br>
             </div>
 
             <fieldset class="row mb-3">
@@ -35,7 +35,7 @@
                         </label>
                     </div>
                 </div>
-            </fieldset>
+            </fieldset><br>
 
 
 
@@ -44,21 +44,22 @@
                 <input type="date" id="data_inicio" name="data_inicio" required><br><br>
 
                 <label for="quantidade_dias">Quantidade de dias de férias:</label>
-                <select class="form-select form-select-lg mb-3" aria-label="Large select example" id="quantidade_dias"
+                <select class="form-select form-select-mb mb-3" aria-label="Large select example" id="quantidade_dias"
                     name="quantidade_dias">
                     <option value="15">15</option>
                     <option value="20">20</option>
                     <option value="30">30</option>
                 </select>
 
-
                 <label for="data_termino">Data de término das férias:</label>
                 <input type="date" id="data_termino" name="data_termino" disabled><br><br>
 
-                <input type="submit" value="Enviar Solicitação">
             </form>
 
             <script>
+                // Lista de datas de feriados (exemplo)
+                const feriados = ["2023-06-01", "2023-07-10", "2023-09-07"];
+
                 document.getElementById('data_inicio').addEventListener('change', function () {
                     calcularDataTermino();
                 });
@@ -78,15 +79,8 @@
                 }
             </script>
 
-
-
-
-
-
-
-
-            <button type="submit" class="btn btn-primary">Enviar Solicitação</button>
-            <p><a href="/index.php">Voltar para a página inicial</a></p>
+            <button type="submit" class="btn btn-primary">Enviar Solicitação</button><br><br>
+            <p><a href="/index.php">Voltar para Home</a></p>
         </form>
     </div>
 
